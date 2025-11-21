af110961ce251f077a899e9e787938ee41022d4d
ALPHA-DEV-BLOCKCHAIN <ibrahimmainainc@gmail.com>
2025-11-21 14:33:09 +0300

    interface AND a class implementation

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,7 +1,16 @@
 namespace Subjects {
+  // Interface for the Subject
   export interface Subject {
     teacher?: Teacher;
     setTeacher(teacher: Teacher): void;
   }
-}
 
+  // Class implementing the interface
+  export class SubjectClass implements Subject {
+    teacher?: Teacher;
+
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
