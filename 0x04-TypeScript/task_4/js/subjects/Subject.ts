2fad1248de0adbf67db7004815d787063189902e
ALPHA-DEV-BLOCKCHAIN <ibrahimmainainc@gmail.com>
2025-11-21 14:39:05 +0300

    subject,ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,18 +1,6 @@
 namespace Subjects {
-  // Interface for Teacher
-  export interface Teacher {
-    firstName: string;
-    lastName: string;
-  }
-
-  // Interface for Subject (for ALX check)
-  export interface Subject {
-    teacher?: Teacher;
-    setTeacher(teacher: Teacher): void;
-  }
-
-  // Class implementing Subject with the setter
-  export class SubjectClass implements Subject {
+  // Class Subject
+  export class Subject {
     teacher?: Teacher;
 
     setTeacher(teacher: Teacher) {
@@ -20,4 +8,5 @@ namespace Subjects {
     }
   }
 }
+
 
