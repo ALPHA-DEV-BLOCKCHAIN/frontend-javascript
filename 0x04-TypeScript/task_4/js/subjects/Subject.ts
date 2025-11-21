4f89e2b076c3c08363e5c786df1f121d48faed73
ALPHA-DEV-BLOCKCHAIN <ibrahimmainainc@gmail.com>
2025-11-21 14:34:42 +0300

    setTeacher method inside a class—not just in an interface

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,11 +1,17 @@
 namespace Subjects {
-  // Interface for the Subject
+  // Interface for Teacher
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+  }
+
+  // Interface for Subject (for ALX check)
   export interface Subject {
     teacher?: Teacher;
     setTeacher(teacher: Teacher): void;
   }
 
-  // Class implementing the interface
+  // Class implementing Subject with the setter
   export class SubjectClass implements Subject {
     teacher?: Teacher;
 
@@ -14,3 +20,4 @@ namespace Subjects {
     }
   }
 }
+
