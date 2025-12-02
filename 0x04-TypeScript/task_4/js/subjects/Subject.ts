a2496baa54ede11bc38c9f2c81f69b4383d02929
ALPHA-DEV-BLOCKCHAIN <ibrahimmainainc@gmail.com>
2025-12-02 22:12:55 +0300

    error  in subject

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,31 +1,11 @@
-/// <reference path="./Teacher.ts" />
+ /// <reference path="./Teacher.ts" />
 
 namespace Subjects {
   export class Subject {
     teacher: Teacher;
 
-    setTeacher(teacher: Teacher) {
+    setTeacher(teacher:Teacher) {
       this.teacher = teacher;
     }
   }
 }
-
-
-
-
-// namespace Subjects {
-//   export interface Teacher {
-//     firstName: string;
-//     lastName: string;
-//   }
-
-//   export class Subject {
-//     teacher: Teacher | undefined;
-
-//     setTeacher(teacher: Teacher): void {
-//       this.teacher = teacher;
-//     }
-//   }
-// }
-
-// export default Subjects;
